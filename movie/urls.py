e462fe7ee367d86cc6651b39ebda2c792859e306
ganeshpaih24 <ganeshpaih24@gmail.com>
2022-12-11 17:03:03 +0530

    updated movie urls.py

--- movie/urls.py
+++ movie/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
+from .views import landing,login,register,search
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    path('', landing, name='landing'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('search/', landing, name='search'),
 ]
