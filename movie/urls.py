2f46e673b526b0ece830c5ca3024ec7289e672b5
Sharan <shettysharan11@gmail.com>
2022-12-17 11:50:03 +0530

    committed login and logout in app user

--- movie/urls.py
+++ movie/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .import views 
 
 urlpatterns = [
-path('login/', views.loginPage, name="login"),
-path('logout/', views.logoutUser, name="logout"),
 
     path('admin/', admin.site.urls),
 
