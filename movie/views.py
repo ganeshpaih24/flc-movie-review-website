abb6be1cbaf7c78c87e2576d54cb5ae9f66b2eb5
ganeshpaih24 <ganeshpaih24@gmail.com>
2022-12-18 16:26:32 +0530

    update movie details

--- movie/views.py
+++ movie/views.py
@@ -75,6 +75,19 @@ def movieDetails(request,imdb_id):
         m.Actors.set(actor_objs)
         m.Ratings.set(rating_objs)
 
+        for actor in actor_objs:
+            actor.movies.add(m)
+            actor.save()
+        m.save()
+        our_db=False
+    context={
+        'movie_data':movie_data,
+        'our_db':our_db,
+    }
+    template=loader.get_template('movie_details.html')
+    return HttpResponse(template.render(context,request))
+
+
 
 def login(request):
     return render(request,'login.html')
