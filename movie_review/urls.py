ed1d48211fd150fccc6e79642c6a3c249b47cf84
Ganesh Pai H <ganeshpaih24@gmail.com>
2022-12-21 16:07:32 +0530

    login and register page working

--- movie_review/urls.py
+++ movie_review/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('movie.urls')),
-    #path('actors/', include('actors.urls')),
-    path('user/', include('user.urls')),
+    path('', include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#extra settings
 
--- user/views.py
+++ user/views.py
@@ -1,83 +1,56 @@
 from django.shortcuts import render
 from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from user.forms import SignupForm,EditProfileForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 
-# Create your views here.
-def loginPage(request):
-
-    if request.method == 'POST':
-        #Get the post parameters
-         username = request.POST.get('username')
-         password = request.POST.get('password')
-
-     #try:
-      #      user = user.objects.get(username=username)
-   # except:
-     #   messages.error(request, 'user does not exist')
-        
-         user = authenticate(request,username=username, password=password)
-
-    if user is not None:
-            login(request,user)
-            messages.success(request, "successfully Logged in")
-            return redirect('home')
-    else:
-            messages.error(request, 'Username or Password does not exist')
-            return redirect('home')
-
-    context = {}
-    return render(request, 'base/login_register.html', context)
-
-def logoutUser(request):
-    logout(request)
-    messages.success(request,"Successfully logged out")
-    return redirect('home')
-
 
 def Signup(request):
-	if request.method == 'POST':
-		form = SignupForm(request.POST)
-		if form.is_valid():
-			form.save()
-            return redirect('login.html')
-        
-	print(form.errors)
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password)
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-	context = {
-		'form': form,
-	}
+    context = {
+        'form': form,
+    }
 
-	return render(request, 'user/register.html', context)
+    return render(request, 'user/register.html', context)
 
 
 @login_required
 def EditProfile(request):
-	user = request.user.id
-	profile = Profile.objects.get(user__id=user)
+    user = request.user.id
+    profile = Profile.objects.get(user__id=user)
 
-	if request.method == 'POST':
-		form = EditProfileForm(request.POST, request.FILES)
-		if form.is_valid():
-			profile.picture = form.cleaned_data.get('picture')
-			profile.first_name = form.cleaned_data.get('first_name')
-			profile.last_name = form.cleaned_data.get('last_name')
-			profile.location = form.cleaned_data.get('location')
-			profile.url = form.cleaned_data.get('url')
-			profile.profile_info = form.cleaned_data.get('profile_info')
-			profile.save()
-			return redirect('index')
-	else:
-		form = EditProfileForm()
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile.picture = form.cleaned_data.get('picture')
+            profile.first_name = form.cleaned_data.get('first_name')
+            profile.last_name = form.cleaned_data.get('last_name')
+            profile.location = form.cleaned_data.get('location')
+            profile.url = form.cleaned_data.get('url')
+            profile.profile_info = form.cleaned_data.get('profile_info')
+            profile.save()
+            return redirect('landing')
+    else:
+        form = EditProfileForm()
 
-	context = {
-		'form': form,
-	}
+    context = {
+        'form': form,
+    }
 
-	return render(request, 'user/edit_profile.html', context)
+    return render(request, 'user/edit_profile.html', context)
 
