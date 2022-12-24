f12bd1b1c6c481bf1d13d4c25d8031eba6c56113
Ganesh Pai H <ganeshpaih24@gmail.com>
2022-12-25 01:23:26 +0530

    minor bug fixes

--- movie/admin.py
+++ movie/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Movie,Genre#, Profile
+from .models import Movie,Genre, Review#, Profile
 
 # admin.site.register(Profile)
 admin.site.register(Movie)
 admin.site.register(Genre)
+admin.site.register(Review)
