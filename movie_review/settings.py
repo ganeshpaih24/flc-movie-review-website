e1409a717c60ec546be8ae7d28be5da604467846
Ganesh Pai H <ganeshpaih24@gmail.com>
2023-01-08 01:09:36 +0530

    server error fix

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR,"movie_review/.env"))
+load_dotenv(os.path.join(BASE_DIR,".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
