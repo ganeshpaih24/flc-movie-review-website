f4fa5cd4952b7860bfcdef4d64efaee1760cea44
Ganesh Pai H <ganeshpaih24@gmail.com>
2023-01-13 17:00:17 +0530

    final touches

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["bit-movies-flc.up.railway.app", "192.168.0.1", "104.196.232.237", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ['https://bit-movies-flc.up.railway.app']
 
 
 # Application definition
